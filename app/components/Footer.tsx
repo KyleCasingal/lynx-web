b2d432446a9661ac00d2b403ee49661cdd238567
KyleCasingal <kylecasingal36@gmail.com>
2024-10-02 15:58:59 +0800

    footer and landing

---
+++ app/components/Footer.tsx
@@ -0,0 +1,96 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import logo from "../img/LB2.png";
+
+export function Footer() {
+  const navigation = ["Service", "Product", "Location", "About"];
+  const legal = ["Terms", "Privacy", "Legal"];
+  return (
+    <div className="relative bg-white">
+      <div>
+        <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
+          <div className="lg:col-span-2">
+            <div>
+              {" "}
+              <Link
+                href="/"
+                className="flex items-center space-x-2 text-2xl font-medium text-black dark:text-black"
+              >
+                <Image src={logo} alt="N" className="w-auto h-20 " />
+              </Link>
+            </div>
+
+            <div className="max-w-md mt-4 text-black">
+              Nextly is a free landing page & marketing website template for
+              startups and indie projects. Its built with Next.js & TailwindCSS.
+              And its completely open-source.
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+              {navigation.map((item, index) => (
+                <Link
+                  key={index}
+                  href="/"
+                  className="w-full px-4 py-2 text-black rounded-md dark:text-black hover:text-indigo-500 focus:text-indigo-500 "
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+              {legal.map((item, index) => (
+                <Link
+                  key={index}
+                  href="/"
+                  className="w-full px-4 py-2 text-black rounded-md dark:text-black hover:text-indigo-500 focus:text-indigo-500 "
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="">
+            <div className="text-black">Follow us</div>
+            <div className="flex mt-5 space-x-5 text-black dark:text-black">
+              <a
+                href="https://facebook.com/web3templates"
+                target="_blank"
+                rel="noopener"
+              >
+                <span className="sr-only">Facebook</span>
+                <Facebook />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-10 text-sm text-center text-black dark:text-black">
+          Copyright © {new Date().getFullYear()}. Lynx Bridge Solutions Inc.{" "}
+        </div>
+      </div>
+
+      {/* Do not remove this */}
+    </div>
+  );
+}
+
+
+const Facebook = ({ size = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M24 12.07C24 5.41 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.8-4.7 4.54-4.7 1.31 0 2.68.24 2.68.24v2.97h-1.5c-1.5 0-1.96.93-1.96 1.89v2.26h3.32l-.53 3.5h-2.8V24C19.62 23.1 24 18.1 24 12.07" />
+  </svg>
+);
+
+
+
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,88 +4,31 @@ import ThemeChanger from "./DarkSwitch";
 import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
 import React from "react";
-import logo from "../img/logo2.png";
+import logo from "../img/LB2.png";
 
 export const Navbar = () => {
-  const navigation = ["Product", "Features", "Pricing", "Company", "Blog"];
+  const navigation = ["Service", "Product", "Location", "About"];
 
   return (
-      <div className="m-0 w-full border-2">
-      <nav className="m-0 container relative flex flex-wrap items-center justify-between mx-auto lg:justify-between xl:px-1">
+    <div className=" m-0 w-full bg-white p-2">
+      <nav className="m-0 relative flex items-center justify-between lg:justify-between xl:px-1">
         {/* Logo  */}
         <Link href="/">
-          <span className="m-0 flex items-center space-x-2 text-2xl font-medium text-black dark:text-gray-100">
+          <span className="m-0 flex items-center text-2xl font-medium text-black dark:text-gray-100">
             <span>
-              <Image
-                src={logo}
-                alt="N"
-                className="w-auto h-16 border"
-              />
+              <Image src={logo} alt="N" className="w-auto h-20 " />
             </span>
           </span>
         </Link>
 
-        <Disclosure>
-          {({ open }: { open: any }) => {
-            return (
-              <>
-                <Disclosure.Button
-                  aria-label="Toggle Menu"
-                  className="px-2 py-1 text-black rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
-                >
-                  <svg
-                    className="w-6 h-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    {open && (
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                      />
-                    )}
-                    {!open && (
-                      <path
-                        fillRule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                      />
-                    )}
-                  </svg>
-                </Disclosure.Button>
-
-                <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
-                  <>
-                    {navigation.map((item, index) => (
-                      <Link
-                        key={index}
-                        href="/"
-                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
-                      >
-                        {item}
-                      </Link>
-                    ))}
-                    <Link
-                      href="/"
-                      className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
-                    >
-                      Get Started
-                    </Link>
-                  </>
-                </Disclosure.Panel>
-              </>
-            );
-          }}
-        </Disclosure>
-
         {/* menu  */}
-        <div className="hidden text-center lg:flex lg:items-center">
-          <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+        <div className=" ">
+          <ul className="flex">
             {navigation.map((menu, index) => (
-              <li className="mr-3 nav__item" key={index}>
+              <li className="m-0 nav__item" key={index}>
                 <Link
                   href="/"
-                  className="inline-block px-4 py-2 text-lg font-normal text-black no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+                  className=" px-4 py-2 text-lg font-normal justify-end text-black no-underline rounded-md dark:text-black hover:text-indigo-500 focus:text-indigo-500 "
                 >
                   {menu}
                 </Link>

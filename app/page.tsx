0941abd50a9a354263da03728638f23c8f4012ed
KyleCasingal <kylecasingal36@gmail.com>
2024-10-04 12:07:26 +0800

    case

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-
-
 import Landing from "./components/Landing";
-
 export default function Home() {
   return (
     <main className="bg-white">
       
       <div className=" m-0 bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]"> 
         <main className="flex flex-col row-start-2 items-start sm:items-start">
-          <Landing/>
+          <Landing />
         </main> 
       </div>
       

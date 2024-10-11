8daf2aa5da5c052e0e3894c6fd7fc3fa393bbad2
KyleCasingal <kylecasingal36@gmail.com>
2024-10-11 12:40:41 +0800

    location info

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -4,7 +4,7 @@ export default function location() {
   return (
     <main className="bg-white">
       <div className=" m-0 bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col row-start-2 items-start sm:items-start">
+        <main className="flex flex-row  justify-between row-start-2 gap-10 items-start sm:items-start">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d482.73306510710904!2d121.18283475253685!3d14.54973901885538!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c1eacbaae5f7%3A0xd22d4121a5b5ca58!2sLynx%20Bridge%20Solution%20Inc.!5e0!3m2!1sen!2sph!4v1728100360003!5m2!1sen!2sph"
             width="800"
@@ -14,6 +14,28 @@ export default function location() {
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
+
+          <div className="flex flex-col items-start ">
+            <div>
+              <p className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Office location
+              </p>
+              <p className="mt-2 text-lg leading-8 text-gray-600">
+                JMC Building Unit D. DOMSA Highway Mahabang Parang, Angono Rizal
+              </p>
+            </div>
+            <div className="mt-10">
+              <p className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Office hours
+              </p>
+              <p className="mt-2 text-lg leading-8 text-gray-600">
+                Monday to Friday 9:00 am to 5:00 pm
+              </p>
+              <p className="mt-2 text-lg leading-8 text-gray-600">
+                Saturday 9:00 am to 3:00 pm
+              </p>
+            </div>
+          </div>
         </main>
       </div>
     </main>

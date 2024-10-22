6c3102bd0617bfffe8d2c770088f5d8e5ac4a7d7
KyleCasingal <kylecasingal36@gmail.com>
2024-10-22 21:15:15 +0800

    navburger

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -46,7 +46,7 @@ const navigation = [
                 className="px-2 py-1 text-black rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
               >
                 <svg
-                  className="w-6 h-6 fill-current"
+                  className="w-6 h-6 fill-slate-800"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
